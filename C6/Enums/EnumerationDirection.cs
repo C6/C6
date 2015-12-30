91d315c68c668967880119f1f914f2c422d808b7
Mikkel Riise Lund <lund.mikkel@gmail.com>
2015-12-30 23:40:16 +0100

    Adds enums for enumeration direction and speed.

---
+++ C6/Enums/EnumerationDirection.cs
@@ -0,0 +1,46 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+
+using System.Diagnostics.Contracts;
+
+namespace C6
+{
+    /// <summary>
+    /// Defines the directions of an enumeration order relative to the original collection.
+    /// </summary>
+    public enum EnumerationDirection : byte
+    {
+        /// <summary>
+        /// Enumeration order is the same as the original collection.
+        /// </summary>
+        Forwards,
+
+        /// <summary>
+        /// Enumeration order is the opposite of the original collection.
+        /// </summary>
+        Backwards,
+    }
+
+
+
+    // TODO: Move to separate file?
+    /// <summary>
+    /// Provides a set of static methods for <see cref="EnumerationDirection"/>.
+    /// </summary>
+    public static class EnumerationDirectionExtension
+    {
+        /// <summary>
+        /// Determines if a <see cref="EnumerationDirection"/> is
+        /// <see cref="EnumerationDirection.Forwards"/>.
+        /// </summary>
+        /// <param name="direction">The <see cref="EnumerationDirection"/> to
+        /// check.</param>
+        /// <returns><c>true</c> if direction is forwards;
+        /// otherwise, <c>false</c>.</returns>
+        [Pure]
+        public static bool IsForward(this EnumerationDirection direction)
+        {
+            return direction == EnumerationDirection.Forwards;
+        }
+    }
+}
---
+++ C6/Enums/Speed.cs
@@ -0,0 +1,37 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+
+namespace C6
+{
+    // TODO: Add information about amortize/expected. See http://docs.scala-lang.org/overviews/collections/performance-characteristics.html
+    // Values are order to be comparable: Constant < Log < Linear < PotentiallyInfinite
+    /// <summary>
+    /// The symbolic characterization of the speed of lookups for a collection.
+    /// </summary>
+    /// <remarks>
+    /// The values may refer to worst-case, amortized and/or expected
+    /// asymptotic complexity with regards to the collection size.
+    /// </remarks>
+    public enum Speed : byte
+    {
+        /// <summary>
+        /// The operation takes constant time.
+        /// </summary>
+        Constant,
+
+        /// <summary>
+        /// The operation may take time proportional to the logarithm of the collection size.
+        /// </summary>
+        Log,
+
+        /// <summary>
+        /// The operation may take time proportional to the collection size.
+        /// </summary>
+        Linear,
+
+        /// <summary>
+        /// The operation might never end.
+        /// </summary>
+        PotentiallyInfinite,
+    }
+}
