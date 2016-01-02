b491cd83408ab66a1be48ef5ace46208291a168d
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-01-02 16:07:25 +0100

    Adds dummy Serialized attribute.

---
+++ C6/Serializable.cs
@@ -0,0 +1,8 @@
+﻿namespace System
+{
+    // TODO: Keep this?
+    /// <summary>
+    /// Dummy attribute to make collections Serializable when compiled as .NET 4.0 project
+    /// </summary>
+    internal sealed class SerializableAttribute : Attribute { }
+}
