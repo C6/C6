4c4e772bf4f5d5437556ca32cbe99594ac8bbc2e
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-01-18 18:04:14 +0100

    Adds tests for enum types.

---
+++ C6.Tests/Enums/EnumerationDirectionExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using C6.Tests.Contracts;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using static C6.EnumerationDirection;
+
+
+
+namespace C6.Tests.Enums
+{
+    [TestFixture]
+    public class EnumerationDirectionExtensionTests
+    {
+        [Test]
+        public void IsForward_Forwards_True() =>
+            Assert.That(Forwards.IsForward(), Is.True);
+
+        [Test]
+        public void IsForward_Backwards_False() =>
+            Assert.That(Backwards.IsForward(), Is.False);
+
+        [Test]
+        public void IsForward_InvalidValue_ViolatesPrecondition()
+        {
+            var invalidEnumerationDirection = (EnumerationDirection) 2;
+
+            Assert.That(() => invalidEnumerationDirection.IsForward(), Violates.Precondition);
+        }
+    }
+}
---
+++ C6.Tests/Enums/SpeedTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using static C6.Speed;
+
+
+
+namespace C6.Tests.Enums
+{
+    [TestFixture]
+    public class SpeedTests
+    {
+        [Test]
+        public void Speed_ConstantIsLessThanLogarithmic_IsTrue() => 
+            Assert.That(Constant, Is.LessThan(Log));
+
+
+        [Test]
+        public void Speed_LogarithmicIsLessThanLinear_IsTrue() => 
+            Assert.That(Log, Is.LessThan(Linear));
+
+
+        [Test]
+        public void Speed_LinearIsLessThanPotentiallyInfinite_IsTrue() => 
+            Assert.That(Linear, Is.LessThan(PotentiallyInfinite));
+    }
+}
