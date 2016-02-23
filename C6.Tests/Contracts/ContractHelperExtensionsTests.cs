8c866f0b5cecfef0ddd5ffa9aa74457b5d9addc0
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-02-23 23:09:42 +0100

    Adds UnsequenceEqual contract helper extension method.

---
+++ C6.Tests/Contracts/ContractHelperExtensionsTests.cs
@@ -0,0 +1,168 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+
+using System;
+using System.Collections;
+using System.Linq;
+
+using NUnit.Framework;
+
+
+namespace C6.Tests.Contracts
+{
+    [TestFixture]
+    public sealed class ContractHelperExtensionsTests
+    {
+        #region UnsequenceEqual TestCases
+
+        private static IEnumerable EqualTestCases => new[] {
+            new TestCaseData(null, null),
+            new TestCaseData(new int[] { }, new int[] { }),
+            new TestCaseData(new[] { 1 }, new[] { 1 }),
+            new TestCaseData(new[] { 1, 1, 1, 1, 1 }, new[] { 1, 1, 1, 1, 1 }),
+            new TestCaseData(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5 }),
+            new TestCaseData(new[] { 1, 2, 3, 4, 5 }, new[] { 5, 4, 3, 2, 1 }),
+            new TestCaseData(new[] { 1, 2, 3, 4, 5 }, new[] { 3, 4, 1, 5, 2 }),
+        };
+
+        private static IEnumerable UnequalTestCases => new[] {
+            new TestCaseData(null, new int[] { }),
+            new TestCaseData(new int[] { }, null),
+            new TestCaseData(new int[] { }, new[] { 1 }),
+            new TestCaseData(new[] { 0 }, new[] { 1 }),
+            new TestCaseData(new[] { 1 }, new[] { 1, 1, 1, 1, 1 }),
+            new TestCaseData(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 3, 4, 5 }),
+            new TestCaseData(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 3, 5 }),
+            new TestCaseData(new[] { 1, 2, 3, 4, 5 }, new[] { 5, 4, 3, 3, 2, 1 }),
+        };
+
+        #endregion
+
+        #region UnsequenceEqual<T>(this SCG.IEnumerable<T>, SCG.IEnumerable<T>)
+
+        [Test]
+        [TestCaseSource(nameof(EqualTestCases))]
+        public void UnsequenceEqual_Equals_True(int[] first, int[] second)
+        {
+            // Act
+            var result = first.UnsequenceEqual(second);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(UnequalTestCases))]
+        public void UnsequenceEqual_NotEquals_False(int[] first, int[] second)
+        {
+            // Act
+            var result = first.UnsequenceEqual(second);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        #endregion
+
+        #region UnsequenceEqual<T>(this SCG.IEnumerable<T>, SCG.IEnumerable<T>, SCG.IEqualityComparer<T>)
+
+        [Test]
+        [TestCaseSource(nameof(EqualTestCases))]
+        public void UnsequenceEqual_CustomComparerEquals_True(int[] first, int[] second)
+        {
+            // Arrange
+            var comparer = ComparerFactory.CreateEqualityComparer((x, y) => x == y, (int x) => x.GetHashCode());
+
+            // Act
+            var result = first.UnsequenceEqual(second, comparer);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(UnequalTestCases))]
+        public void UnsequenceEqual_CustomComparerNotEquals_False(int[] first, int[] second)
+        {
+            // Arrange
+            var comparer = ComparerFactory.CreateEqualityComparer((x, y) => x == y, (int x) => x.GetHashCode());
+
+            // Act
+            var result = first.UnsequenceEqual(second, comparer);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        [TestCase(new[] { "" }, new[] { "" }, true)]
+        [TestCase(new[] { "" }, new[] { "Hello" }, false)]
+        [TestCase(new[] { "Why", "Go", "Hello", "World", "Every time?" }, new[] { "Hallo", "Welt", "Wie", "Geht", "Es?" }, true)]
+        public void UnsequenceEqual_IntialComparer_True(string[] first, string[] second, bool expectedResult)
+        {
+            // Arrange
+            Func<string, string> firstLetterOrEmpty = s => string.IsNullOrEmpty(s) ? string.Empty : s.Substring(0, 1);
+            Func<string, string, bool> equals = (x, y) => firstLetterOrEmpty(x).Equals(firstLetterOrEmpty(y));
+            Func<string, int> getHashCode = x => firstLetterOrEmpty(x).GetHashCode();
+            var comparer = ComparerFactory.CreateEqualityComparer(equals, getHashCode);
+
+            // Act
+            var result = first.UnsequenceEqual(second, comparer);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EqualTestCases))]
+        public void UnsequenceEqual_EqualHashCodesEquals_True(int[] first, int[] second)
+        {
+            // Arrange
+            Func<int, int, bool> equals = (x, y) => x == y;
+            Func<int, int> getEqualHashCode = x => 0;
+            var comparer = ComparerFactory.CreateEqualityComparer(equals, getEqualHashCode);
+
+            // Act
+            var result = first.UnsequenceEqual(second, comparer);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(UnequalTestCases))]
+        public void UnsequenceEqual_EqualHashCodesNotEquals_False(int[] first, int[] second)
+        {
+            // Arrange
+            Func<int, int, bool> equals = (x, y) => x == y;
+            Func<int, int> getEqualHashCode = x => 0;
+            var comparer = ComparerFactory.CreateEqualityComparer(equals, getEqualHashCode);
+
+            // Act
+            var result = first.UnsequenceEqual(second, comparer);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void UnsequenceEqual_LargeEquals_True()
+        {
+            // Arrange
+            const int count = 10000;
+            var random = TestContext.CurrentContext.Random;
+            var randomStrings = Enumerable.Range(0, count).Select(i => random.GetString()).ToList();
+            var first = randomStrings.ToList();
+            var second = randomStrings.ToList();
+            second.Shuffle(random);
+
+            // Act
+            var result = first.UnsequenceEqual(second);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        #endregion
+    }
+}
--- C6/Contracts/ContractHelperExtensions.cs
+++ C6/Contracts/ContractHelperExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// This file is part of the C6 Generic Collection Library for C# and CLI
 // See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
 
+using System;
+using System.Diagnostics.Contracts;
 using System.Linq;
 
 using SCG = System.Collections.Generic;
@@ -39,6 +41,94 @@ namespace C6
             // ReSharper disable PossibleMultipleEnumeration
             return enumerable.Take(startIndex).Concat(enumerable.Skip(startIndex + count));
             // ReSharper enable PossibleMultipleEnumeration
+        }
+
+        /// <summary>
+        /// Determines whether two enumerables contain the same elements in
+        /// regards to multiplicity, but not sequence order, using the default
+        /// equality comparer for their type.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the input
+        /// sequences.</typeparam>
+        /// <param name="first">An <see cref="SCG.IEnumerable{T}"/> to compare
+        /// to <paramref name="second"/>.</param>
+        /// <param name="second">An <see cref="SCG.IEnumerable{T}"/> to compare
+        /// to <paramref name="first"/>.</param>
+        /// <returns><c>true</c> if the enumerables contain equal items;
+        /// otherwise, <c>false</c>.</returns>
+        [Pure]
+        public static bool UnsequenceEqual<T>(this SCG.IEnumerable<T> first, SCG.IEnumerable<T> second)
+        {
+            return UnsequenceEqual(first, second, SCG.EqualityComparer<T>.Default);
+        }
+
+        /// <summary>
+        /// Determines whether two enumerables contain the same elements in
+        /// regards to multiplicity, but not sequence order, using a specified
+        /// <see cref="SCG.IEqualityComparer{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the input
+        /// sequences.</typeparam>
+        /// <param name="first">An <see cref="SCG.IEnumerable{T}"/> to compare
+        /// to <paramref name="second"/>.</param>
+        /// <param name="second">An <see cref="SCG.IEnumerable{T}"/> to compare
+        /// to <paramref name="first"/>.</param>
+        /// <param name="comparer">An <see cref="SCG.IEqualityComparer{T}"/> to
+        /// use to compare elements.</param>
+        /// <returns><c>true</c> if the enumerables contain equal items;
+        /// otherwise, <c>false</c>.</returns>
+        [Pure]
+        public static bool UnsequenceEqual<T>(this SCG.IEnumerable<T> first, SCG.IEnumerable<T> second, SCG.IEqualityComparer<T> comparer)
+        {
+            if (ReferenceEquals(first, second)) {
+                return true;
+            }
+
+            if (first == null || second == null) {
+                return false;
+            }
+
+            var firstArray = first as T[] ?? first.ToArray();
+            var secondArray = second as T[] ?? second.ToArray();
+
+            if (firstArray.Length != secondArray.Length) {
+                return false;
+            }
+
+            // Use default comparer if none is supplied
+            comparer = comparer ?? SCG.EqualityComparer<T>.Default;
+            
+            // Sort based on hash code
+            Comparison<T> hashCodeComparison = (x, y) => comparer.GetHashCode(x).CompareTo(comparer.GetHashCode(y));
+            Array.Sort(firstArray, hashCodeComparison);
+            Array.Sort(secondArray, hashCodeComparison);
+            
+            for (var i = 0; i < firstArray.Length; i++) {
+                var found = false;
+                var firstElement = firstArray[i];
+
+                for (var j = i; j < secondArray.Length; j++) {
+                    var secondElement = secondArray[j];
+                    
+                    if (hashCodeComparison(firstElement, secondElement) != 0) {
+                        break;
+                    }
+
+                    if (comparer.Equals(firstElement, secondElement)) {
+                        secondArray.Swap(i, j);
+
+                        // Continue with next element in first array
+                        found = true;
+                        break;
+                    }
+                }
+                
+                if (!found) {
+                    return false;
+                }
+            }
+            
+            return true;
         }
     }
 }
