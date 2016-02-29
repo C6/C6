9fd9fedec5f7c8208236a572d9ecfa9c1f9bfcac
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-02-29 18:28:01 +0100

    Makes ArrayList<T> implement IEnumerable<out T>. Adds failing test.

--- C6.Tests/Collections/ArrayListTests.cs
+++ C6.Tests/Collections/ArrayListTests.cs
@@ -1,6 +1,8 @@
 ﻿// This file is part of the C6 Generic Collection Library for C# and CLI
 // See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
 
+using SCG = System.Collections.Generic;
+
 using NUnit.Framework;
 
 
@@ -9,5 +11,25 @@ namespace C6.Tests
     [TestFixture]
     public class ArrayListTests
     {
+        #region Helper Methods
+
+        private static SCG.IEnumerable<int> GetEmptyList() => new ArrayList<int>();
+
+        #endregion
+
+        #region IEnumerable<T>
+
+        [Test]
+        public void GetEnumerator_EmptyCollection_Empty()
+        {
+            // Arrange
+            var collection = GetEmptyList();
+
+            // Assert
+            Assert.That(collection, Is.Empty);
+        }
+
+        #endregion
+
     }
 }
--- C6/Collections/ArrayList.cs
+++ C6/Collections/ArrayList.cs
@@ -1,7 +1,23 @@
 ﻿// This file is part of the C6 Generic Collection Library for C# and CLI
 // See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
 
+using System;
+
+using SCG = System.Collections.Generic;
+
+
 namespace C6
 {
-    public class ArrayList<T> {}
+    public class ArrayList<T> : SCG.IEnumerable<T>
+    {
+        public SCG.IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
 }
