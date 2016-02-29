d95258fab3ddc2ed6945c7c6139373b8bbd85d35
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-02-29 17:40:47 +0100

    Adds empty ArrayList<T> class and ArrayListTests test class.

---
+++ C6.Tests/Collections/ArrayListTests.cs
@@ -0,0 +1,13 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+
+using NUnit.Framework;
+
+
+namespace C6.Tests
+{
+    [TestFixture]
+    public class ArrayListTests
+    {
+    }
+}
---
+++ C6/Collections/ArrayList.cs
@@ -0,0 +1,7 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+
+namespace C6
+{
+    public class ArrayList<T> {}
+}
