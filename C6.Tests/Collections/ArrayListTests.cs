392f7d70d0b230afa86be373d50b1508798d9b9f
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-03-21 11:32:22 +0100

    Adds new ICollectionTests class and makes ArrayList<T> inherit from it.

--- C6.Tests/Collections/ArrayListTests.cs
+++ C6.Tests/Collections/ArrayListTests.cs
@@ -17,7 +17,7 @@ using SCG = System.Collections.Generic;
 namespace C6.Tests
 {
     [TestFixture]
-    public class ArrayListTests : IExtensibleTests
+    public class ArrayListTests : ICollectionTests
     {
         #region Helper Methods
 
@@ -265,10 +265,11 @@ namespace C6.Tests
         protected override bool DuplicatesByCounting => false;
         protected override bool IsFixedSize => false;
         protected override bool IsReadOnly => false;
+        protected override Speed ContainsSpeed => Speed.Linear;
 
-        protected override IExtensible<T> GetEmptyExtensible<T>(SCG.IEqualityComparer<T> equalityComparer = null, bool allowsNull = false) => new ArrayList<T>(equalityComparer: equalityComparer, allowsNull: allowsNull);
+        protected override ICollection<T> GetEmptyCollection<T>(SCG.IEqualityComparer<T> equalityComparer = null, bool allowsNull = false) => new ArrayList<T>(equalityComparer: equalityComparer, allowsNull: allowsNull);
 
-        protected override IExtensible<T> GetExtensible<T>(SCG.IEnumerable<T> enumerable, SCG.IEqualityComparer<T> equalityComparer = null, bool allowsNull = false)
+        protected override ICollection<T> GetCollection<T>(SCG.IEnumerable<T> enumerable, SCG.IEqualityComparer<T> equalityComparer = null, bool allowsNull = false)
             => new ArrayList<T>(enumerable, equalityComparer, allowsNull);
     }
 }
---
+++ C6.Tests/Interfaces/ICollectionTests.cs
@@ -0,0 +1,65 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+
+using System;
+using System.Linq;
+
+using C6.Tests.Collections;
+using C6.Tests.Contracts;
+using C6.Tests.Helpers;
+
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+using static C6.Contracts.ContractMessage;
+using static C6.Tests.Helpers.CollectionEvent;
+using static C6.Tests.Helpers.TestHelper;
+
+using SCG = System.Collections.Generic;
+
+namespace C6.Tests
+{
+    [TestFixture]
+    public abstract class ICollectionTests : IExtensibleTests
+    {
+        #region Factories
+
+        protected abstract Speed ContainsSpeed { get; }
+
+        protected abstract ICollection<T> GetEmptyCollection<T>(SCG.IEqualityComparer<T> equalityComparer = null, bool allowsNull = false);
+
+        protected abstract ICollection<T> GetCollection<T>(SCG.IEnumerable<T> enumerable, SCG.IEqualityComparer<T> equalityComparer = null, bool allowsNull = false);
+
+        #region Helpers
+
+        private ICollection<int> GetIntCollection(Random random, SCG.IEqualityComparer<int> equalityComparer = null, bool allowsNull = false)
+            => GetCollection(GetIntegers(random, GetCount(random)), equalityComparer, allowsNull);
+
+        private ICollection<int> GetIntCollection(Random random, int count, SCG.IEqualityComparer<int> equalityComparer = null, bool allowsNull = false)
+            => GetCollection(GetIntegers(random, count), equalityComparer, allowsNull);
+
+        private ICollection<string> GetStringCollection(Randomizer random, SCG.IEqualityComparer<string> equalityComparer = null, bool allowsNull = false)
+            => GetCollection(GetStrings(random, GetCount(random)), equalityComparer, allowsNull);
+
+        private ICollection<string> GetStringCollection(Randomizer random, int count, SCG.IEqualityComparer<string> equalityComparer = null, bool allowsNull = false)
+            => GetCollection(GetStrings(random, count), equalityComparer, allowsNull);
+
+        #endregion
+
+        #region Inherited
+
+        protected override IExtensible<T> GetEmptyExtensible<T>(SCG.IEqualityComparer<T> equalityComparer = null, bool allowsNull = false) => GetEmptyCollection(equalityComparer, allowsNull);
+
+        protected override IExtensible<T> GetExtensible<T>(SCG.IEnumerable<T> enumerable, SCG.IEqualityComparer<T> equalityComparer = null, bool allowsNull = false) => GetCollection(enumerable, equalityComparer, allowsNull);
+
+        #endregion
+
+        #endregion
+
+        #region Test Methods
+
+
+
+        #endregion
+    }
+}
