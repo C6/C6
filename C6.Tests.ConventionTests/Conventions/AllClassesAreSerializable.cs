65e47844ef9bd6722bc7e7c340329b44d128271c
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-03-25 13:10:42 +0100

    Adds convention tests project along with first test ensuring that all classes are serializable.

---
+++ C6.Tests.ConventionTests/Conventions/AllClassesAreSerializable.cs
@@ -0,0 +1,33 @@
+// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+
+using System;
+using System.Linq;
+
+using TestStack.ConventionTests;
+using TestStack.ConventionTests.ConventionData;
+
+
+namespace C6.Tests.ConventionTests
+{
+    public class AllClassesAreSerializable : IConvention<Types>
+    {
+        public static AllClassesAreSerializable In => new AllClassesAreSerializable();
+
+        public void Execute(Types data, IConventionResultContext result)
+        {
+            var nonSerializableTypes = data.Where(type => !type.IsSerializable &&
+                                                          !type.IsInterface &&
+                                                          !IsStatic(type) &&
+                                                          !type.IsCompilerGenerated() &&
+                                                          !type.Name.Equals("SerializableAttribute"));
+
+            result.Is("Classes must be serializable", nonSerializableTypes);
+        }
+
+        [Obsolete("Should be replaced with type.IsStatic() - https://github.com/TestStack/TestStack.ConventionTests/issues/73")]
+        private static bool IsStatic(Type type) => type.IsClass & type.IsSealed & type.IsAbstract;
+
+        public string ConventionReason => "All classes should be serializable: https://github.com/sestoft/C5/issues/8";
+    }
+}
---
+++ C6.Tests.ConventionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("C6.Tests.ConventionTests.dll")]
+[assembly: AssemblyDescription("Convention tests ensure that the C6 library and its tests conform to its own conventions.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Niels Kokholm, Peter Sestoft, Rasmus Lystrøm, Mikkel Riise Lund")]
+[assembly: AssemblyProduct("C6")]
+[assembly: AssemblyCopyright("Copyright © Niels Kokholm, Peter Sestoft, Rasmus Lystrøm, Mikkel Riise Lund 2003-2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a793fd91-ceeb-4e92-93db-36de8d1583f6")]
