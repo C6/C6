18de3d4082d327ca6e5445cfba9761ab694cb477
Mikkel Riise Lund <mirl@itu.dk>
2016-04-17 14:48:47 +0200

    Changes license header to point to new repo.

--- C6/SequencedEqualityComparer.cs
+++ C6/SequencedEqualityComparer.cs
@@ -1,5 +1,5 @@
 ﻿// This file is part of the C6 Generic Collection Library for C# and CLI
-// See https://github.com/lundmikkel/C6/blob/master/LICENSE.md for licensing details.
+// See https://github.com/C6/C6/blob/master/LICENSE.md for licensing details.
 
 using System.Diagnostics.Contracts;
 using System.Linq;
