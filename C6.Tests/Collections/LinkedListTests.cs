985e9bc4713e04636c38973c99d2bc80c1592f36
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-06-04 22:43:45 +0200

    Adds LinkedList implementing IEnumerable<T>.

---
+++ C6.Tests/Collections/LinkedListTests.cs
@@ -0,0 +1,20 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/C6/C6/blob/master/LICENSE.md for licensing details.
+
+using C6.Collections;
+
+using NUnit.Framework;
+
+using SCG = System.Collections.Generic;
+
+
+namespace C6.Tests.Collections
+{
+    [TestFixture]
+    public class LinkedListTests : IEnumerableTests
+    {
+        protected override SCG.IEnumerable<T> GetEmptyEnumerable<T>() => new LinkedList<T>();
+
+        protected override SCG.IEnumerable<T> GetEnumerable<T>(SCG.IEnumerable<T> enumerable) => new LinkedList<T>(enumerable);
+    }
+}
---
+++ C6/Collections/LinkedList.cs
@@ -0,0 +1,201 @@
+﻿// This file is part of the C6 Generic Collection Library for C# and CLI
+// See https://github.com/C6/C6/blob/master/LICENSE.md for licensing details.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+using C6.Contracts;
+
+using SC = System.Collections;
+using SCG = System.Collections.Generic;
+
+using static System.Diagnostics.Contracts.Contract;
+
+using static C6.Collections.ExceptionMessages;
+using static C6.Contracts.ContractMessage;
+
+
+namespace C6.Collections
+{
+    /// <summary>
+    ///     Represents a generic doubly linked list.
+    /// </summary>
+    [Serializable]
+    public class LinkedList<T> : SCG.IEnumerable<T>
+    {
+        #region Fields
+
+        private readonly Node _first, _last;
+        private int _version;
+
+        #endregion
+
+        #region Code Contracts
+
+        [ContractInvariantMethod]
+        // [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Required for code contracts.")]
+        private void ObjectInvariant()
+        {
+            // ReSharper disable InvocationIsSkipped
+
+            // _first is never null
+            Invariant(_first != null);
+
+            // The node before the first is always null
+            Invariant(_first.Previous == null);
+
+            // _last is never null
+            Invariant(_last != null);
+
+            // The node after the last is always null
+            Invariant(_last.Next == null);
+
+            // List is equal forwards and backwards
+            Invariant(EnumerateFrom(_first.Next).IsSameSequenceAs(EnumerateBackwardsFrom(_last.Previous).Reverse()));
+
+            // ReSharper restore InvocationIsSkipped
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public LinkedList()
+        {
+            _first = new Node(default(T));
+            _last = new Node(default(T));
+
+            _first.Next = _last;
+            _last.Previous = _first;
+        }
+
+        public LinkedList(SCG.IEnumerable<T> items) : this()
+        {
+            var previous = _first;
+            foreach (var item in items) {
+                previous = new Node(item, previous, _last);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public SCG.IEnumerator<T> GetEnumerator() => EnumerateFrom(_first.Next).GetEnumerator();
+
+        #endregion
+
+        #region Explicit Implementations
+
+        SC.IEnumerator SC.IEnumerable.GetEnumerator() => GetEnumerator();
+
+        #endregion
+
+        #region Private Methods
+
+        [Pure]
+        private bool CheckVersion(int version)
+        {
+            if (version == _version) {
+                return true;
+            }
+
+            // See https://msdn.microsoft.com/library/system.collections.ienumerator.movenext.aspx
+            throw new InvalidOperationException(CollectionWasModified);
+        }
+
+        [Pure]
+        private SCG.IEnumerable<T> EnumerateBackwardsFrom(Node cursor)
+        {
+            #region Code Contracts
+
+            // Argument must be non-null
+            Requires(cursor != null, ArgumentMustBeNonNull);
+
+            // Node cannot be the last sentinel node
+            Requires(cursor != _last);
+
+            // The version is not updated
+            Ensures(_version == OldValue(_version));
+
+            #endregion
+
+            var version = _version;
+
+            // Check version at each call to MoveNext() to ensure an exception is thrown even when the enumerator was really finished
+            while (CheckVersion(version) & cursor != _first) {
+                yield return cursor.Item;
+                cursor = cursor.Previous;
+            }
+        }
+
+        [Pure]
+        private SCG.IEnumerable<T> EnumerateFrom(Node cursor)
+        {
+            #region Code Contracts
+
+            // Argument must be non-null
+            Requires(cursor != null, ArgumentMustBeNonNull);
+
+            // Node cannot be the first sentinel node
+            Requires(cursor != _first);
+
+            // The version is not updated
+            Ensures(_version == OldValue(_version));
+
+            #endregion
+
+            var version = _version;
+
+            // Check version at each call to MoveNext() to ensure an exception is thrown even when the enumerator was really finished
+            while (CheckVersion(version) & cursor != _last) {
+                yield return cursor.Item;
+                cursor = cursor.Next;
+            }
+        }
+
+        #endregion
+
+        #region Nested Types
+
+        /// <summary>
+        ///     Represents an individual cell in the linked list.
+        /// </summary>
+        [Serializable]
+        private class Node
+        {
+            public Node Previous, Next;
+            public T Item;
+
+            public Node(T item)
+            {
+                Item = item;
+            }
+
+            public Node(T item, Node previous)
+            {
+                Item = item;
+
+                // Set previous' pointers
+                Previous = previous;
+                previous.Next = this;
+            }
+
+            public Node(T item, Node previous, Node next)
+            {
+                Item = item;
+
+                // Set previous' pointers
+                Previous = previous;
+                previous.Next = this;
+
+                // Set next's
+                Next = next;
+                next.Previous = this;
+            }
+        }
+
+        #endregion
+    }
+}
