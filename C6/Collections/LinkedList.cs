f782776cf54d4ee25e068f363b8d2ded9b87e1b3
Mikkel Riise Lund <lund.mikkel@gmail.com>
2016-06-19 14:47:49 +0200

    Removes CheckVersion() from collection values because it isn't required.

--- C6/Collections/LinkedList.cs
+++ C6/Collections/LinkedList.cs
@@ -1280,7 +1280,7 @@ namespace C6.Collections
                     return false;
                 }
 
-                while (CheckVersion()) {
+                while (true) {
                     // Check if enumerator is done
                     if (!_enumerator.MoveNext()) {
                         // Set enumerator to null to indicate that the base has been fully enumerated
@@ -1295,9 +1295,6 @@ namespace C6.Collections
                         return true;
                     }
                 }
-
-                // This is never executed as CheckVersion() throws an exception instead of returning false
-                return false;
             }
 
             #endregion
@@ -1461,7 +1458,7 @@ namespace C6.Collections
                 }
 
                 // Check if enumerator is done
-                if (CheckVersion() & !_enumerator.MoveNext()) {
+                if (!_enumerator.MoveNext()) {
                     // Set enumerator to null to indicate that the base has been fully enumerated
                     _enumerator = null;
 
